ac93f28b904066c08b8360f045096ccc38f81237
Daegonny <igorchagasm@hotmail.com>
2019-11-07 14:32:30 -0300

    added feature to draw heart lives

---
+++ Assets/LiveSscript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LiveSscript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject player;
+    private djScript playerScript;
+    public Sprite one;
+    public Sprite two;
+    public Sprite three;
+    private SpriteRenderer sr;
+    void Start()
+    {
+        playerScript = player.GetComponent<djScript>();
+        sr = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerScript.lives == 3){
+            sr.sprite = three;
+        }
+        else if(playerScript.lives == 2){
+            sr.sprite = two;
+        }
+        else{
+            sr.sprite = one;
+        }
+    }
+}
--- Assets/Scripts/djScript.cs
+++ Assets/Scripts/djScript.cs
@@ -3,23 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class djScript : MonoBehaviour{
-    public float rotationSpeed = 3.0f;
-    public float speed;
-    private Rigidbody2D rb2d;
-    private Vector2 moveVelocity;
+   
+    private CircleCollider2D cc2d;
+    public int lives = 3;
     void Start()
     {
-        rb2d = GetComponent<Rigidbody2D>();
+        cc2d = GetComponent<CircleCollider2D>();
     }
 
     void Update() {
-        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        moveVelocity = moveInput.normalized * speed;
-        transform.Rotate(new Vector3(0, 0, 45) * Time.deltaTime*rotationSpeed);
+        
     }
 
     private void FixedUpdate()
     {
-        rb2d.MovePosition(rb2d.position + moveVelocity * Time.fixedDeltaTime);
+        
     }
 }
