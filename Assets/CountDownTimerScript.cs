b141b5afb51ae496a3aa5b048d677128bb0dbfe2
joao-psp <Joao14725joao>
2019-11-10 13:05:03 -0300

    Add cena de Gâanho, e timer de 60s

---
+++ Assets/CountDownTimerScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CountDownTimerScript : MonoBehaviour
+{
+    public Text uiText;
+    public float mainTimer;
+
+    private float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = mainTimer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timer >= 0.0f) {
+            timer -= Time.deltaTime;
+            uiText.text = timer.ToString("F");
+        } else {
+            uiText.text = "0.0";
+            SceneManager.LoadScene(2);
+        }
+    }
+}
