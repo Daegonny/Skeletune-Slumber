38a4157c5524aafeee6179ea96f5573313923b97
joao-psp <Joao14725joao>
2019-11-25 22:29:58 -0300

    Add Tela de Menu e ajustando score e timer

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void goesToGameScene() {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+        Debug.Log("dkjfnsdkjfn");
+    }
+
+    public void goesToTutorialScene() {
+         Debug.Log("goesToTutorial");
+    }
+
+    public void goesToAboutScene() {
+         Debug.Log("goesToTutorial");
+    }
+}
