2aea908e4555d5e0a5d1c359d1fa2c3a70aa9200
Paulo <phsilva2309@gmail.com>
2019-12-04 10:50:25 -0300

    Pause Menu

--- Assets/AudioManager.cs
+++ Assets/AudioManager.cs
@@ -23,10 +23,25 @@ public class AudioManager : MonoBehaviour {
         Play("Theme", true);
     }
 
+
     public void Play (string name, bool loop = false) {
         Sound s =  Array.Find(sounds, sound => sound.name == name);
         if(loop) s.source.loop = true;
         
+        s.source.Play();
+    }
+
+    public void PauseTheme()
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == "Theme");
+
+        s.source.Pause();
+    }
+
+    public void ResumeTheme()
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == "Theme");
+
         s.source.Play();
     }
 }
